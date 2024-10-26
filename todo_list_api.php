304565ac0cec38b33b240d84f966f3885b090d7b
sidharth-kb <sidharthkbiju@gmail.com>
2024-10-26 14:07:19 +0100

    todo list api - tested

--- todo_list_api.php
+++ todo_list_api.php
@@ -1,13 +1,13 @@
-<?php
 
 <?php
 // Set headers to allow CORS and JSON response
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+//header("Access-Control-Allow-Methods: POST");
 
-require_once('./todo.php')
+require_once('./todo.php');
 
+/*
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get the JSON input data
@@ -30,4 +30,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     http_response_code(405); // Method Not Allowed
     echo json_encode(["error" => "Only POST requests are allowed"]);
 }
-?>
+ */
+
+// Set headers to allow CORS and JSON response
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// Check if the required parameters are present
+if (isset($_GET['employee_id'])) {
+    $employee_id = htmlspecialchars($_GET['employee_id']);
+
+    // Example response
+    $response = getUserTasks($employee_id);
+
+    // Send a JSON response
+    echo json_encode($response);
+} else {
+    http_response_code(400); // Bad request
+    echo json_encode(["error" => "Missing required parameters"]);
+}
